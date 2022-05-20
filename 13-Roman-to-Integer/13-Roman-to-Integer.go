e541258611ea5d448eb81db1979f56572dd69190
m1dabo <mohammed.dabo@hotmail.com>
2022-05-20 23:11:17 +0200

    This is the solution of translating  the Roman letters to Integers

--- 13-Roman-to-Integer/13-Roman-to-Integer.go
+++ 13-Roman-to-Integer/13-Roman-to-Integer.go
@@ -1,6 +1,31 @@
 package RomanToInteger
 
+var romanLetters = map[string]int{
+	"I": 1,
+	"V": 5,
+	"X": 10,
+	"L": 50,
+	"C": 100,
+	"D": 500,
+	"M": 1000,
+}
+
 func romanToInt(s string) int {
-	//Any return before solving the problem
-	return 0
+	if s == "" {
+		return 0
+	}
+
+	currentNum, lastNum, result := 0, 0, 0
+	s_len := len(s)
+	for i := 0; i < s_len; i++ {
+		character := s[s_len-(i+1) : s_len-i]
+		currentNum = romanLetters[character]
+		if currentNum < lastNum {
+			result = result - currentNum
+		} else {
+			result = result + currentNum
+		}
+		lastNum = currentNum
+	}
+	return result
 }
