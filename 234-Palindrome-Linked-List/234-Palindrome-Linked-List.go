26f833e2f79c16e1e0d6ee297e4982a8c95a39d6
m1dabo <mohammed.dabo@hotmail.com>
2022-05-21 03:52:19 +0200

    Solving Palindrome-linked-list Problem

---
+++ 234-Palindrome-Linked-List/234-Palindrome-Linked-List.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+package palindromelinkedlist
+
+//Singly LinkedList
+type ListNode struct {
+	data int
+	next *ListNode
+}
+
+// type linkedList struct {
+// 	head   *ListNode
+// 	length int
+// }
+
+// func (l *linkedList) prepend(n *ListNode) {
+// 	second := l.head
+// 	l.head.next = second
+// 	l.length++
+// }
+
+func isPalindrome(head *ListNode) bool {
+	
+	return false
+}
