95c9c32e3d4caa8c12bf8bbb356c809f5f5a251f
m1dabo <mohammed.dabo@hotmail.com>
2022-05-22 06:52:16 +0200

    not a complete solution of the problem I'll continue later

--- 383-Ransom-Note/383-Ransom-Note.go
+++ 383-Ransom-Note/383-Ransom-Note.go
@@ -1,5 +1,31 @@
 package ransomnote
 
 func canConstruct(ransomNote string, magazine string) bool {
-    
+	rn_len := len(ransomNote)
+	m_len := len(magazine)
+
+	map_test := make(map[string]int)
+
+	if rn_len == 0 || m_len == 0 {
+		return false
+	}
+
+	counter := 0
+	for i := 0; i < rn_len; i++ {
+		current_letter := string(ransomNote[i])
+		map_test[current_letter] = 1
+		counter = 0
+		for j := 0; j < m_len; j++ {
+			if ransomNote[i] == magazine[j] {
+				counter++
+				map_test[current_letter] = map_test[current_letter] + 1
+			}
+		}
+		_, isPresent := map_test[string(ransomNote[i])]
+		if counter == 0 || isPresent {
+			return false
+		}
+	}
+
+	return true
 }
